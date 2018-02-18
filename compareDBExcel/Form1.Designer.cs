6890dc0d27a8e3523cfb732349a425272bd2ef90
MrBeekle <patkruz@gmail.com>
2018-02-18 13:01:16 +0100

    first commit
    vvvf

---
+++ compareDBExcel/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace compareDBExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(238, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(35, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(41, 143);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(538, 292);
+            this.listBox1.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Choose Excel file to compare:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(41, 82);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 33);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Compare";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(279, 45);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(313, 20);
+            this.listBox2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(604, 457);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ compareDBExcel/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LinqToExcel;
+
+
+namespace compareDBExcel
+{
+    public partial class Form1 : Form
+    {
+        string excelFilePath;
+        List<string> compareList = new List<string>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                listBox2.Items.Add(openFileDialog1.FileName);
+
+            excelFilePath = openFileDialog1.FileName;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Pobieranie danych z pliku excel
+            string sheetName = "1.2 Cash";
+
+            var excelFile = new ExcelQueryFactory(excelFilePath);
+            var getThisData = from a in excelFile.Worksheet(sheetName) where (a["Is used"].Cast<int>() > 0 && (a["R"].Cast<int>() < 0 || a["R"].Cast<int>() > 0)) select a;
+            /*
+            *   a["Is used"].Cast<int>() > 0 pobiera wartosci z kolumny is used wieksze od zera czyli tam gdzie jest wartosc
+            * 
+            *   a["R"].Cast<int>() < 0 pobiera warosc z kolumny R tam gdzie jest znaczek funta(checkox nie zaznaczony) -> sprawdzic to 
+            */
+
+            foreach (var a in getThisData)
+            {
+                //dodanie do listy 'test' przefiltrowanych wedlug warunku z lini 19 danych z 3 kolumny arkusza
+                compareList.Add(a[2]);
+                listBox1.Items.Add(a[2]);
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
